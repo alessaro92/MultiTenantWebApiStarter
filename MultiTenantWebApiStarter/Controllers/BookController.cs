27a73dd4a1e0cf472621608696764f68f0c30dcb
alessaro92 <alerex92@hotmail.it>
2024-08-03 00:53:48 +0200

    Added controller for querying/managing book table through NH session

---
+++ MultiTenantWebApiStarter/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiTenantWebApiStarter.Entities;
+using MultiTenantWebApiStarter.Helpers;
+
+namespace MultiTenantWebApiStarter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            using NHibernate.ISession session = NHibernateHelper.GetSession();
+            return session.Query<Book>().ToList();
+        }
+    }
+}
