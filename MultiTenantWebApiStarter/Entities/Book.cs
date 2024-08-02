4568e7b618e2246f8b4d70906b98a44ddd865701
alessaro92 <alerex92@hotmail.it>
2024-08-03 00:16:20 +0200

    Added entity and mapping classes for the book table

---
+++ MultiTenantWebApiStarter/Entities/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTenantWebApiStarter.Entities
+{
+    public class Book
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Author { get; set; }
+    }
+}
---
+++ MultiTenantWebApiStarter/Mappings/BookMap.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+using MultiTenantWebApiStarter.Entities;
+
+namespace MultiTenantWebApiStarter.Mappings
+{
+    public class BookMap : ClassMap<Book>
+    {
+        public BookMap()
+        {
+            Table("book");
+            LazyLoad();
+            Id(x => x.Id).Column("id").GeneratedBy.Assigned();
+            Map(x => x.Title).Column("title").Not.Nullable().Length(50);
+            Map(x => x.Author).Column("author").Not.Nullable().Length(100);
+        }
+    }
+}
